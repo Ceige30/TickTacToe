894c84e251c824031b60ab2826fd0326152069f8
cm921 <sig91308@gmail.com>
2016-12-16 14:29:27 -0500

    The link is live

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,10 @@
 package com.company;
-
+import java.util.*;
 public class Main {
-
+//http://www.poll-maker.com/poll924782x770F468E-38
     public static void main(String[] args) {
-
+       Scanner sc = new Scanner(System.in);
+        int place1 = sc.nextInt();
+        int place2 = sc.nextInt();
     }
 }
--- src/com/company/locationGrid.java
+++ src/com/company/locationGrid.java
@@ -2,6 +2,6 @@ package com.company;
 
 public class locationGrid {
     public int getLocation(int df, int dg){
-        
+
     }
 }
