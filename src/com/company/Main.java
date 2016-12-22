f8d4f4880b4e11b2c44ea48d70ae1ad1f589fa1a
cm921 <sig91308@gmail.com>
2016-12-22 14:26:03 -0500

    Well just one more problem on the pile also hi Mr. P Who plays Hansel in Zoolander? The dim-witted but good-natured Derek Zoolander (Ben Stiller) is ousted as the top male fashion model by the rising star, Hansel McDonald (Owen Wilson), and his reputation is further tarnished by a critical article from journalist Matilda Jeffries (Christine Taylor).

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,13 +4,37 @@ public class Main {
 //http://www.poll-maker.com/poll924782x770F468E-38
     public static void main(String[] args) {
        Scanner sc = new Scanner(System.in);
-        String filler [] = {"_","_","_","_","_","_","_","_","_"};
-        String row1 = "|___"+filler[0]+"___|___"+filler[1]+"___|___"+filler[2]+"___|\n";
-        String row2 = "|___"+filler[3]+"___|___"+filler[4]+"___|___"+filler[5]+"___|\n";
-        String row3 = "|___"+filler[6]+"___|___"+filler[7]+"___|___"+filler[8]+"___|";
-        String choice = "";
+        String filler [][] = {{" "," "," "},
+                              {" "," "," "},
+                              {" "," "," "}};
+        String row1 = "     |     |     \n"+
+                      "  "+filler[0][0]+"  |  "+filler[0][1]+"  |  "+filler[0][2]+"  \n"+
+                      "_____|_____|_____";
+        String row2 = "     |     |     \n"+
+                      "  "+filler[1][0]+"  |  "+filler[1][1]+"  |  "+filler[1][2]+"  \n"+
+                      "_____|_____|_____";
+        String row3 = "     |     |     \n"+
+                      "  "+filler[0][0]+"  |  "+filler[0][1]+"  |  "+filler[0][2]+"  \n"+
+                      "     |     |     ";
+        boolean choice;
+        String answer = "";
+        System.out.println(row1);
+        System.out.println(row2);
+        System.out.println(row3);
      System.out.println("Hello and welcome to Tic Tac Toe would you like the game to be player vs. player, player vs. computer, or computer vs. computer");
-     System.out.println("Please type pvp to have player vs. player pvc for playr vs. computer and cvc for computer vs. computer");
-
+     System.out.println("Please type pvp to have player vs. player pvc for player vs. computer and cvc for computer vs. computer");
+        while(choice = true){
+            answer = sc.nextLine();
+            System.out.println(answer);
+            if(answer.equals("pvp")) {
+                choice = true;
+            }
+            else if(answer.equals("pvc"))
+                choice = true;
+            else if(answer.equals("cvc"))
+                choice = true;
+            System.out.println("done loop");
+        }
+        System.out.println("done");
     }
 }
--- src/com/company/locationGird.java
+++ src/com/company/locationGird.java
@@ -1,10 +1,9 @@
-package com.company;
+/*package com.company;
 
-/**
- * Created by cm921 on 12/19/16.
- */
+
 public class locationGird {
     public getLocation() {
 
     }
 }
+*/
